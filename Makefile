9f8e12b9b446654e8480ace14b1015bc654462d0
trevorsuper <129561006+trevorsuper@users.noreply.github.com>
2025-10-23 14:41:59 -0400

    initial commit

---
+++ Makefile
@@ -0,0 +1,21 @@
+cc = gcc
+debug_flags = -Wall -Wextra -Werror -O0 -g -MMD -MP -std=c17
+debug_sanitizer_flags = -Wall -Wextra -Werror -O0 -fsanitize=undefined,address -march=native -MMD -MP -std=c17
+optimized_flags = -Wall -Wextra -Werror -O3 -march=native -MMD -MP -std=c17
+source = weather.c
+target = weather
+
+# Run make clean before switching between different types of
+# a build, debug, sanitized, optimized, etc.
+
+debug: $(source)
+	$(cc) $(debug_flags) -c $(source)
+	$(cc) -o $(target) *.o
+san: $(source)
+	$(cc) $(debug_sanitizer_flags) -c $(source)
+	$(cc) -o $(target) *.o -fsanitize=undefined,address
+opt: $(source)
+	$(cc) $(release_flags) -c $(source)
+	$(cc) -o $(target) *.o
+clean:
+	rm *.d *.o $(target)
---
+++ weather.c
@@ -0,0 +1,29 @@
+//#include <curl/curl.h>
+#include <stdio.h>
+
+int main(){
+//int main(int argc, char *argv[]){
+	printf("******************************************************************************\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("*                                                                            *\n");
+	printf("******************************************************************************\n");
+    return 0;
+}
