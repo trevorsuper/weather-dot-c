eb726fcf401c76f98e198d63a117b107bf76f47f
trevorsuper <129561006+trevorsuper@users.noreply.github.com>
2025-10-26 20:47:41 -0400

    Add WMO Weather Interpretation Codes

---
+++ src/weather.c
@@ -0,0 +1,19 @@
+//#include <curl/curl.h>
+#include <stdio.h>
+
+int main(){
+//int main(int argc, char *argv[]){
+	enum weather_code = {
+		clear_sky = 0, mainly_clear = 1, partly_cloudy = 2, overcast = 3,
+		fog = 45, depositing_rime_fog = 48, light_drizzle = 51, 
+		moderate_drizzle = 53, dense_drizzle = 55, light_freezing_drizzle = 56,
+		dense_freezing_drizzle = 57, slight_rain = 61, moderate_rain = 63, 
+		heavy_rain = 65, light_freezing_rain = 66, heavy_freezing_rain = 67, 
+		slight_snowfall = 71, moderate_snowfall = 73, heavy_snowfall = 75, 
+		snow_grains = 77, slight_rain_showers = 80, moderate_rain_showers = 81, 
+		violent_rain_showers = 82, slight_snow_showers = 85, 
+		heavy_snow_showers = 86, thunderstorm = 95, 
+		thunderstorm_with_slight_hail = 96, thunderstorm_with_heavy_hail = 99
+	};	
+    return 0;
+}
